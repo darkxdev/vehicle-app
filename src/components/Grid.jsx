80c931dfad741a08add8cd7066d7bf192fe412dd
DarkxDev <bryanfernandohurtado@gmail.com>
2023-03-14 10:12:30 -0500

    Added search bar to be able to filter categories

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { fetchMakes } from '../redux/makesSlice';
 
 const Grid = () => {
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState('');
+
+  const makes = useSelector((state) => state.makes.makes);
+  const makesCount = useSelector((state) => state.makes.count);
+
+  useEffect(() => {
+    if (makes.length === 0) {
+      dispatch(fetchMakes());
+    }
+  });
+
   const categories = [
     {
       category: 'Makes',
       path: '/makes',
       id: '1',
+      count: makesCount,
     },
     {
       category: 'Manufacturers',
@@ -14,13 +30,25 @@ const Grid = () => {
     },
   ];
 
+  const filteredCategories = categories.filter((category) =>
+    category.category.toLowerCase().includes(query.toLowerCase())
+  );
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <div>
       <h2>Grid</h2>
+      <input type="text" placeholder="Search categories" value={query} onChange={handleQueryChange} />
       <ul>
-        {categories.map((category) => (
+        {filteredCategories.map((category) => (
           <li key={category.id}>
-            <NavLink to={category.path}>{category.category}</NavLink>
+            <NavLink to={category.path}>
+              {category.category}
+              {category.count && <p>{category.count}</p>}
+            </NavLink>
           </li>
         ))}
       </ul>
--- src/components/Makes.jsx
+++ src/components/Makes.jsx
@@ -1,16 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMakes } from '../redux/makesSlice';
+import { useSelector } from 'react-redux';
 
 const Makes = () => {
-  const dispatch = useDispatch();
   const makes = useSelector((state) => state.makes.makes);
-
-  useEffect(() => {
-    if (makes.length === 0) {
-      dispatch(fetchMakes());
-    }
-  });
 
   return (
     <div className="makes-page">
